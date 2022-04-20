ca01970de3b46f518ad8058979716e45fd3543c3
SodaCris <wang-jiaxun@outlook.com>
2022-04-20 15:20:15 +0800

    modify doc, remove sql

--- sql/prepration.sql
+++
@@ -1,4 +0,0 @@
-create database if not exists bwc;
-CREATE USER IF NOT EXISTS 'lanzhou_client'@'%' IDENTIFIED WITH mysql_native_password BY 'ferVId428?=';
-GRANT SELECT, INSERT, UPDATE, DELETE, DROP ON bwc.* TO 'lanzhou_client'@'%';
-FLUSH PRIVILEGES;
